664de89621b00a4d66ce4a494720ce13a3eaf04e
vanessaBonito <vanebonito@hotmail.com>
2022-08-18 22:06:17 -0500

    Create AddBookFrm component

--- src/components/AddBookFrm.js
+++ src/components/AddBookFrm.js
@@ -1,5 +1,14 @@
-const AddBookFrm = () => {
+import React from 'react';
 
-};
+const AddBookFrm = () => (
+  <div>
+    <h2>ADD NEW BOOK</h2>
+    <form>
+      <input type="text" placeholder="Book title" />
+      <input type="text" placeholder="Author" />
+      <button type="submit">ADD BOOK</button>
+    </form>
+  </div>
+);
 
 export default AddBookFrm;
