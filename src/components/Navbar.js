b02825431c64754338fdfbcf13887657c0482cc4
vanessaBonito <vanebonito@hotmail.com>
2022-08-18 22:31:41 -0500

    Create Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,22 @@
-const Navbar = () => {
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-};
+const Navbar = () => (
+  <nav>
+    <span>
+      <a href="/">
+        Bookstore CMS
+      </a>
+    </span>
+    <ul>
+      <li>
+        <Link to="/">Books</Link>
+      </li>
+      <li>
+        <Link to="/categories">Categories</Link>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Navbar;
