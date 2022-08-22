46259d1902c52258d85c0547491e6e19e16a3645
vanessaBonito <vanebonito@hotmail.com>
2022-08-22 17:31:35 -0500

    Add reducer function in /src/redux/books/books.js

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,8 +1,36 @@
-//Action types
-const ADD_BOOK = "redux/books/ADD_BOOK";
-const REMOVE_BOOK = "redux/books/REMOVE_BOOK";
+// Action types
+const ADD_BOOK = 'redux/books/ADD_BOOK';
+const REMOVE_BOOK = 'redux/books/REMOVE_BOOK';
 
-//Initialize the state
+// Create and export action creators
+export const AddBook = (newBook) => ({
+  type: ADD_BOOK,
+  book: newBook,
+});
+
+export const RemoveBook = (idBook) => ({
+  type: REMOVE_BOOK,
+  id: idBook,
+});
+
+// Initialize the state
 const initialState = {
-    books: []
-  };
+  books: [],
+};
+
+// Reducer
+const reducer = (action) => {
+  let { books } = initialState;
+  switch (action.type) {
+    case ADD_BOOK:
+      books.push(action.book);
+      return { ...initialState, books };
+    case REMOVE_BOOK:
+      books = books.filter((book) => book.id !== action.id);
+      return { ...initialState, books };
+    default:
+      return initialState;
+  }
+};
+
+export default reducer;
