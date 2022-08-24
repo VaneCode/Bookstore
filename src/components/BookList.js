99817514d861e1149c0d7482554fa6f60027eab7
vanessaBonito <vanebonito@hotmail.com>
2022-08-23 20:14:33 -0500

    Dispatch ADD_BOOK action in AddBookFrm

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,9 @@
-import { useSelector } from 'react-redux/es/exports';
+import { useSelector, shallowEqual } from 'react-redux/es/exports';
 import Book from './Book';
 
 const BookList = () => {
   // Get the list of books from the state
-  const booksList = useSelector((state) => state.books.value);
+  const booksList = useSelector((state) => state.books, shallowEqual);
   return (
     <div>
       {booksList.map((data) => (
