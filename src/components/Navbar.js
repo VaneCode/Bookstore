476f22cb4cd9163f2ed665a521bf22b16f5dc593
vanessaBonito <vanebonito@hotmail.com>
2022-08-26 12:39:53 -0500

    Add style to logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
+import styles from '../styles/Navbar.module.css';
 
 const Navbar = () => (
   <nav>
     <span>
-      <a href="/">
+      <a href="/" className={styles.BookstoreCMS}>
         Bookstore CMS
       </a>
     </span>
