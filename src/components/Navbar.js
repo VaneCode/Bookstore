7fffd132ab2c0e1ad6f0babd350c6803c821fc72
vanessaBonito <vanebonito@hotmail.com>
2022-08-26 17:13:17 -0500

    Add style to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,55 @@
+import { useState } from 'react';
+import { MdClose } from 'react-icons/md';
+import { FiMenu } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import styles from '../styles/Navbar.module.css';
 
-const Navbar = () => (
-  <nav>
-    <span>
-      <a href="/" className={styles.BookstoreCMS}>
-        Bookstore CMS
-      </a>
-    </span>
-    <ul>
-      <li>
-        <Link to="/">Books</Link>
-      </li>
-      <li>
-        <Link to="/categories">Categories</Link>
-      </li>
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const handleToggle = () => {
+    setNavbarOpen((prev) => !prev);
+  };
+  // const closeMenu = () => {
+  // setNavbarOpen(false);
+  // };
+  return (
+    <nav className={styles.NavBar}>
+      <div>
+        <Link to="/" className={styles.BookstoreCMS}>
+          Bookstore CMS
+        </Link>
+      </div>
+      <ul className={styles.navigation}>
+        <li>
+          <button onClick={handleToggle} type="button" className={styles.BurgerIcon}>
+            {navbarOpen ? (
+              <MdClose
+                style={{ color: '#7b7b7b', width: '1rem', height: '1rem' }}
+              />
+            ) : (
+              <FiMenu
+                style={{ color: '#7b7b7b', width: '1rem', height: '1rem' }}
+              />
+            )}
+          </button>
+        </li>
+        <li>
+          <ul className={styles.nav}>
+            <li>
+              <Link to="/" className={styles.navItem}>
+                BOOKS
+              </Link>
+            </li>
+            <li>
+              <Link to="/categories" className={styles.navItem}>
+                CATEGORIES
+              </Link>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
